594854a3a0c56b187797471656b44cca24e8c28c
Talvinder Singh <101984603+Talvi0905@users.noreply.github.com>
2022-04-01 16:37:37 +0530

    area of square

---
+++ areaof.py
@@ -0,0 +1,69 @@
+#area of square
+#side * side
+
+print("area of square")
+
+side1=int(input("Enter the side 1 :"))
+side2=int(input("Enter the side 2 :"))
+
+area=side1*side2
+
+print(f"Area of square : {area} sq m2")
+
+#area of Triagle
+#1/2*base*height
+
+print("area of Triagle")
+
+base=int(input("Enter the base :"))
+height=int(input("Enter the height :"))
+
+tri_area=1/2*base*height
+
+print(f"Area of Triagle : {tri_area} sq m2")
+
+
+#Area of Rectangle
+#base * height
+
+print("Area of Rectangle")
+
+rbase=int(input("Enter the base :"))
+rheight=int(input("Enter the height :"))
+
+rec_area=rbase*rheight
+
+print(f"Area of Rectangle : {rec_area} sq m2")
+
+
+#Areaa of Circle
+#pi_value*redius*redius
+
+print("Areaa of Circle")
+
+pi_value2=22/7
+pi_value=int(pi_value2)
+
+
+redius=int(input("Enter the Redius :"))
+
+cir_area=pi_value*redius*redius
+
+print(f"Area of Circle : {cir_area} sq m2")
+
+
+#Area of Ellipes
+#pi_value*a_axis*b_axis
+
+print("Area of Ellipes")
+
+a_axis=int(input("Enter the Axis a :"))
+basec=int(input("Enter the Axis b :"))
+
+elli_area=pi_value*a_axis*basec
+
+print(f"Area of Circle : {elli_area} sq m2")
+
+
+input("enter to Exit")
+#print"Press Enter to Exit"
